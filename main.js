029e3cc95a6bcb20782e5607486e06cf974d7671
Chris Yu <chris_yu@epam.com>
2016-06-02 14:54:55 +0800

    render html

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-app.get('/', function(request, response) {
-  response.send('hello home page');
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+
+app.get('*', function(req, res){
+    res.render('index.html');
 });
 
-app.listen(8010, function() {
-  console.log('Node app is running on port ' + 8010);
+app.listen(app.get('port'), function() {
+  console.log('listening on ' + app.get('port'));
 });
